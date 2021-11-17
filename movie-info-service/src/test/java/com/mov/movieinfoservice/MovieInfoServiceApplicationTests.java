3b105fe3af8193bd413afb12522ee01e5043f15f
Kumar Shubhendu <kumarshubhendu27@gmail.com>
2021-11-17 22:55:40 +0530

    01. Added the three api

---
+++ movie-info-service/src/test/java/com/mov/movieinfoservice/MovieInfoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mov.movieinfoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieInfoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
